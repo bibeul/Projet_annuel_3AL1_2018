7b067fea49de8d1d970548be7ffc6bb8ca304cd0
dormoy <dormoy.guillaume@gmail.com>
2018-06-19 11:24:03 +0200

    add controller for map play and plugin

---
+++ client_lourd/src/main/java/Interface/Controller/PlayController.java
@@ -0,0 +1,36 @@
+package Interface.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlayController {
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    private Stage stage;
+
+    public void hello(){
+
+    }
+    public void mapManagement(ActionEvent event) throws IOException {
+        FXMLDocumentController fdc = new FXMLDocumentController();
+        fdc.switchingScene(event,"mapManagement");
+    }
+
+    public void pluginManagement(ActionEvent event) throws IOException {
+        FXMLDocumentController fdc = new FXMLDocumentController();
+        fdc.switchingScene(event,"pluginManagement");
+    }
+
+    public void playView(ActionEvent event) throws IOException {
+        FXMLDocumentController fdc = new FXMLDocumentController();
+        fdc.switchingScene(event,"play");
+    }
+}
