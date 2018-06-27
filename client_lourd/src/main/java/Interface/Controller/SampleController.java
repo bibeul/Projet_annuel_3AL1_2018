1995b0aa6e3cee222e76b470fbb0e40569586d7f
dormoy <dormoy.guillaume@gmail.com>
2018-06-27 23:26:34 +0200

    uncollapsable titledpane

--- client_lourd/src/main/java/Interface/Controller/SampleController.java
+++ client_lourd/src/main/java/Interface/Controller/SampleController.java
@@ -1,11 +1,23 @@
 package Interface.Controller;
 
+import Interface.util.TestConstant;
+import Interface.util.plugin.PluginLoader;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SampleController {
+
+    @FXML
+    private TitledPane sampleTitledPane;
+
+    public void initialize() throws Exception {
+        sampleTitledPane.setCollapsible(false);
+    }
+
     public Stage getStage() {
         return stage;
     }
